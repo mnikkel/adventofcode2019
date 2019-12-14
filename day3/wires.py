1d1d0bfdda4b78230b04c45d3d4be970e5bbc50c
Mark Nikkel <mnikkel@gmail.com>
2019-12-14 14:32:41 -0500

    day 3 part 1

---
+++ day3/wires.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+GRID_SIZE = 20000
+START = 10000
+
+WIRES = []
+CROSSED = []
+DISTANCE = []
+with open('input') as f:
+    for line in f:
+        WIRES.append(line.rstrip())
+
+W1 = WIRES[0].split(',')
+W2 = WIRES[1].split(',')
+
+GRID = [[None for _ in range(GRID_SIZE)] for _ in range(GRID_SIZE)]
+
+
+def place_wire(wire, name):
+    pos = [START, START]
+    key = {'U': [-1, 0], 'D': [1, 0], 'L': [0, -1], 'R': [0, 1]}
+    for link in wire:
+        direction = link[:1]
+        distance = int(link[1:])
+        for _ in range(distance):
+            pos = [x + y for x, y in zip(key[direction], pos)]
+            if GRID[pos[0]][pos[1]] is None:
+                GRID[pos[0]][pos[1]] = name
+            elif GRID[pos[0]][pos[1]] != name:
+                CROSSED.append([pos[0]-START, pos[1]-START])
+                GRID[pos[0]][pos[1]] = 'cross'
+
+
+place_wire(W1, 'w1')
+place_wire(W2, 'w2')
+
+for coord in CROSSED:
+    DISTANCE.append(abs(coord[0]) + abs(coord[1]))
+
+print(min(DISTANCE))
